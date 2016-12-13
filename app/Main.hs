42de336ee79f0435bc817416234a71acfd130264
Amar Shah <amar47shah@gmail.com>
2016-12-13 00:17:52 -0500

    Implement a stateful, factor-multiplying server

--- app/Main.hs
+++ app/Main.hs
@@ -1,37 +1,87 @@
-import Control.Concurrent (forkFinally)
-import Control.Monad (forever)
-import GHC.Conc.Sync (ThreadId)
-import Network
-import System.IO
-import Text.Printf
+import Control.Applicative (liftA2)
+import Control.Concurrent (ThreadId, forkFinally)
+import Control.Concurrent.Async (race)
+import Control.Concurrent.STM.TChan (TChan, newTChan, readTChan, writeTChan)
+import Control.Concurrent.STM.TVar (TVar, newTVar, readTVar, writeTVar)
+import Control.Monad (forever, join, void)
+import Control.Monad.STM (STM, atomically)
+import Network (HostName, PortID (PortNumber), PortNumber, Socket, accept, listenOn, withSocketsDo)
+import System.IO (Handle, BufferMode (LineBuffering), hClose, hGetLine, hPutStrLn, hSetBuffering)
+import Text.Printf (hPrintf, printf)
 
-main = withSocketsDo $
-  listen port >>= \sock ->
-    logListening port *>
-      forever
-        (accept sock >>= logAcceptance *> communicateOn . fst3)
+type TFactor = TVar Integer
 
-port :: Int
-port = 44444
+main :: IO ()
+main = withSocketsDo $ do
+  socket  <- listenOn' port
+  tFactor <- atomically initializeTFactor
+  forever $ connect socket tFactor
 
-listen :: Int -> IO Socket
-listen = listenOn . PortNumber . fromIntegral
+connect :: Socket -> TFactor -> IO ThreadId
+connect s tf = accept' s >>= forkServer tf
 
-logListening :: Int -> IO ()
-logListening = printf "Listening on port %d\n"
+-- Networking
 
-logAcceptance :: (PrintfArg b, Show c) => (a, b, c) -> IO ()
-logAcceptance (_, h, p) = printf "Accepted connection from %s: %s\n" h $ show p
+port :: PortID
+port = PortNumber 44444
 
-communicateOn :: Handle -> IO GHC.Conc.Sync.ThreadId
-communicateOn = forkFinally <$> talk <*> const . hClose
+listenOn' :: PortID -> IO Socket
+listenOn' p = listenOn p >>= \socket -> logListening p *> pure socket
 
-talk :: Handle -> IO ()
-talk h = hSetBuffering h LineBuffering *> loop
-    where
-  loop = hGetLine h >>= respond
-  respond "end" = hPutStrLn h "Thank you for using the Haskell doubling service."
-  respond line  = hPutStrLn h (show $ 2 * (read line :: Integer)) *> loop
+accept' :: Socket -> IO Handle
+accept' s = accept s >>= \(handle, h, p) -> logAcceptance h p *> pure handle
 
-fst3 :: (a, b, c) -> a
-fst3 (x, _, _) = x
+logListening :: PortID -> IO ()
+logListening = printf "Listening on port %s\n" . show
+
+logAcceptance :: HostName -> PortNumber -> IO ()
+logAcceptance h p = printf "Accepted connection from %s: %s\n" h $ show p
+
+-- Serving
+
+initializeTFactor :: STM (TFactor)
+initializeTFactor = newTVar 2
+
+forkServer :: TFactor -> Handle -> IO ThreadId
+forkServer tf = forkFinally <$> serve tf <*> const . hClose
+
+serve :: TFactor -> Handle -> IO ()
+serve tf h =
+  hSetBuffering h LineBuffering *>
+    atomically newTChan >>=
+      void . (race <$> respond tf h <*> receive h)
+
+receive :: Handle -> TChan String -> IO ()
+receive h c = forever $ hGetLine h >>= atomically . writeTChan c
+
+respond :: TFactor -> Handle -> TChan String -> IO ()
+respond tf h c =
+  atomically (readTVar tf) >>=
+    liftA2 (*>) (hPrintf h "Starting factor: %d\n") (loop tf h c)
+
+loop :: TFactor -> Handle -> TChan String -> Integer -> IO ()
+loop tf h c = join . atomically . react tf h c
+
+react :: TFactor -> Handle -> TChan String -> Integer -> STM (IO ())
+react tf h c f =
+  readTVar tf >>= \fNow ->
+    if f == fNow
+    then command tf h c f <$> readTChan c
+    else pure $ hPrintf h "New factor: %d\n" fNow *> loop tf h c fNow
+
+command :: TFactor -> Handle -> TChan String -> Integer -> String -> IO ()
+command _  h _ _ "end"   = end h
+command tf h c f ('*':s) = changeFactor tf s *> loop tf h c f
+command tf h c f s       = multiply h f    s *> loop tf h c f
+
+changeFactor :: TFactor -> String -> IO ()
+changeFactor tf = atomically . writeTVar tf . readInteger
+
+multiply :: Handle -> Integer -> String -> IO ()
+multiply h f = hPutStrLn h . show . (f *) . readInteger
+
+end :: Handle -> IO ()
+end h = hPutStrLn h "Thank you for using the Haskell multiplying service."
+
+readInteger :: String -> Integer
+readInteger = read
